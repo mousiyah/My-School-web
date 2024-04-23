7a8de85a38b32f89b0bcc5cbf94b30e0cb33cbc1
mousiyah <muslimakarimova@gmail.com>
2024-04-23 17:26:49 +0100

    works

--- server/models/attendance.js
+++ server/models/attendance.js
@@ -1,39 +1,40 @@
-'use strict';
-const { Model } = require('sequelize');
+"use strict";
+const { Model } = require("sequelize");
 module.exports = (sequelize, DataTypes) => {
   class attendance extends Model {
     static associate(models) {
-
       attendance.belongsTo(models.lesson, {
         foreignKey: {
-          name: 'lessonId',
+          name: "lessonId",
           allowNull: false,
-          onUpdate: 'CASCADE',
-          onDelete: 'CASCADE',
-        }
+          onUpdate: "CASCADE",
+          onDelete: "CASCADE",
+        },
       });
 
       attendance.belongsTo(models.student, {
         foreignKey: {
-          name: 'studentId',
+          name: "studentId",
           allowNull: false,
-          onUpdate: 'CASCADE',
-          onDelete: 'CASCADE',
-        }
+          onUpdate: "CASCADE",
+          onDelete: "CASCADE",
+        },
       });
-
     }
   }
-  attendance.init({
-    attended: {
-      type: DataTypes.BOOLEAN,
-      allowNull: true
+  attendance.init(
+    {
+      attended: {
+        type: DataTypes.BOOLEAN,
+        allowNull: true,
+      },
+    },
+    {
+      sequelize,
+      timestamps: true,
+      modelName: "attendance",
+      tableName: "attendances",
     }
-  }, {
-    sequelize,
-    timestamps: true,
-    modelName: 'attendance',
-    tableName: 'attendances',
-  });
+  );
   return attendance;
 };
